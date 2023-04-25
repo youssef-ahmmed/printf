3d79c456cf873e02bf92e4600df12871a7b311c2
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-04-26 01:19:42 +0200

    Add base_converter file

---
+++ base_converter.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * convert_num_to_string - converts a number to a string
+ * @num: number to convert
+ * @base: base to convert to
+ * @flags: flags
+ * @params: parameters
+ * 
+ * Return: converted number
+*/
+char *convert_num_to_string(long int num, int base, int flags, params_t *params)
+{
+    static char buffer[50];
+    char sign = 0;
+    char *numbered_string = &buffer[49];
+    unsigned long n = num;
+    char *array;
+
+    UNUSED(params);
+
+    if (!(flags & CONVERT_UNSIGNED) && num < 0)
+    {
+        n = -num;
+        sign = '-';
+    }
+
+    if (flags & CONVERT_LOWERCASE)
+        array = "0123456789abcdef";
+    else
+        array = "0123456789ABCDEF";
+
+    *numbered_string = '\0';
+
+    while (n != 0)
+    {
+        *--numbered_string = array[n % base];
+        n /= base;
+    }
+
+    if (sign)
+        *--numbered_string = sign;
+
+    return (numbered_string);
+}
