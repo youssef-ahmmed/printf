3839d764dd19997cc12644aca2d2268daae9e5e1
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-04-26 16:26:10 +0200

    Modify error conditions of printf

--- _printf.c
+++ _printf.c
@@ -16,6 +16,10 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL)
 		return (-1);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	for (curr = (char *) format; *curr; curr++)
 	{
 		init_params(&params, args);
